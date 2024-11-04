dfa407f78740962452f77271974f79c6dc90061f
Berdigylyc <146063996+Berdigylyc@users.noreply.github.com>
2024-11-04 23:15:44 +0300

    Update 2.py

--- LabaratoryWork_Matplotlib/2.py
+++ LabaratoryWork_Matplotlib/2.py
@@ -1,26 +1,27 @@
 import matplotlib.pyplot as plt
 
-def read_time_series(file_path):
-    with open(file_path, 'r') as file:
+def read(path):
+    with open(path, 'r') as file:
         lines = file.readlines() 
         return [list(map(float, line.split())) for line in lines]
 
 def plot(series):
    
-    num_frames = len(series) // 2  
-    for i in range(num_frames):
+    number = len(series) // 2  
+    for i in range(number):
       
         x = series[i * 2]  
         y = series[i * 2 + 1]  
         
       
         plt.plot(x, y, marker='o', markersize=1)  
-        plt.title(f"Frame {i + 1}") 
+        plt.title(f"freym {i + 1}") 
         plt.xlim(min(x) - 5, max(x) + 5)  
         plt.ylim(min(y) - 5, max(y) + 5) 
         plt.grid()  
+        plt.savefig(f"C:/Python/freym_{i + 1}.png") 
         plt.show()
-        plt.savefig("Frame")
-file_path = 'C:/Python/series.txt' 
-series = read_time_series(file_path) 
+        plt.clf()
+path = 'C:/Python/series.txt' 
+series = read(path) 
 plot(series) 
